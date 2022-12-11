8818fc6568f4b78c33c93c8b171d4cb3667a3030
Eugene <zgainer@gmail.com>
2022-12-11 09:37:48 +0300

    Release

--- MySpringAnimation/Model/AnimationModel.swift
+++ MySpringAnimation/Model/AnimationModel.swift
@@ -9,6 +9,7 @@ import Foundation
 import SpringAnimation
 
 struct AnimationSettings {
+    
     let preset: String
     let curve: String
     let force: CGFloat
@@ -28,11 +29,12 @@ struct AnimationSettings {
 
 extension AnimationSettings {
     static func getRandomSettings() -> AnimationSettings {
+        
         let animationSetting = AnimationSettings(preset: AnimationPreset.allCases.randomElement()?.rawValue ?? "fall",
-                                                curve: AnimationCurve.allCases.randomElement()?.rawValue ?? "spring",
-                                                force: CGFloat.random(in: 0.1...2),
-                                                duration: CGFloat.random(in: 1...3),
-                                                delay: CGFloat.random(in: 0...1))
+                                                 curve: AnimationCurve.allCases.randomElement()?.rawValue ?? "spring",
+                                                 force: CGFloat.random(in: 0.1...2),
+                                                 duration: CGFloat.random(in: 1...3),
+                                                 delay: CGFloat.random(in: 0...1))
         
         return animationSetting
     }
